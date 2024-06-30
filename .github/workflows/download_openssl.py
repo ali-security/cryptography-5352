b550157e3e9ccac5978f8d18a2ccd252bb7839d9
Sagi Shumer <sagi.s@sealsecurity.io>
2024-06-30 10:25:44 +0300

    CI

--- .github/workflows/download_openssl.py
+++ .github/workflows/download_openssl.py
@@ -1,27 +1,11 @@
 import io
-import os
 import sys
 import zipfile
 
-import requests
 
-
-RUNS_URL = "https://www.openssl.org/source/old/1.1.1/openssl-{version}"
-
-
-def get_response(url):
-    response = requests.get(url)
-    if response.status_code != 200:
-        raise ValueError("Got HTTP {} fetching {}: ".format(
-            response.code, url, response.content
-        ))
-    return response
-
-
-def main(version):
-    print("Looking for: {}".format(version))
-    response = get_response(RUNS_URL.format(version=version)).json()
-    zipfile.ZipFile(io.BytesIO(response.content)).extractall("C:/{}".format("OpenSSL"))
+def main(arch):
+    with open(r".\openssl_zips\openssl_{}".format(arch), 'rb') as fd:
+        zipfile.ZipFile(io.BytesIO(fd.read())).extractall("C:/{}".format("OpenSSL"))
 
 
 if __name__ == "__main__":
