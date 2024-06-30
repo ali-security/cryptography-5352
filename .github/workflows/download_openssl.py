97e8c8235d9ae5879799e9c9c5811d4133855f78
Sagi Shumer <sagi.s@sealsecurity.io>
2024-06-30 14:53:36 +0300

    change extract path

--- .github/workflows/download_openssl.py
+++ .github/workflows/download_openssl.py
@@ -5,7 +5,7 @@ import zipfile
 
 def main(arch):
     with open(r".github\workflows\openssl_zips\openssl_{}.zip".format(arch), 'rb') as fd:
-        zipfile.ZipFile(io.BytesIO(fd.read())).extractall("C:/{}".format("OpenSSL"))
+        zipfile.ZipFile(io.BytesIO(fd.read())).extractall(".github\workflows\openssl_zips")
 
 
 if __name__ == "__main__":
