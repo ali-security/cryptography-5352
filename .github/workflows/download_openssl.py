3a3a3745375185a10aad3efaadd9019a5eaa9f93
Sagi Shumer <sagi.s@sealsecurity.io>
2024-07-04 16:10:33 +0300

    change paths to hopefully something better

--- .github/workflows/download_openssl.py
+++ .github/workflows/download_openssl.py
@@ -1,12 +1,14 @@
 import io
 import sys
+import shutil
 import zipfile
 
 
-def main(arch):
+def main(arch, windows):
     with open(r".github\workflows\openssl_zips\openssl_{}.zip".format(arch), 'rb') as fd:
         zipfile.ZipFile(io.BytesIO(fd.read())).extractall("C:\\")
+    shutil.move("C:\\OpenSSL", "C:\\OpenSSL-{}".format(windows))
 
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main(sys.argv[1], sys.argv[2])
