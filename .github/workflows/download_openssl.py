ef0a210e23915c7f3381948011d46f4d2927b5b2
Sagi Shumer <sagi.s@sealsecurity.io>
2024-07-04 09:26:08 +0300

    trying things differently

--- .github/workflows/download_openssl.py
+++ .github/workflows/download_openssl.py
@@ -5,7 +5,7 @@ import zipfile
 
 def main(arch):
     with open(r".github\workflows\openssl_zips\openssl_{}.zip".format(arch), 'rb') as fd:
-        zipfile.ZipFile(io.BytesIO(fd.read())).extractall(".github\workflows\openssl_zips")
+        zipfile.ZipFile(io.BytesIO(fd.read())).extractall("C:\\")
 
 
 if __name__ == "__main__":
