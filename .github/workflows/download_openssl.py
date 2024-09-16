618c0266183ea91705af1dc74571ba7cec52d810
Sagi Shumer <sagi.s@sealsecurity.io>
2024-09-16 11:12:02 +0300

    Dsa

--- .github/workflows/download_openssl.py
+++ .github/workflows/download_openssl.py
@@ -73,4 +73,6 @@ def main(platform, target):
 if __name__ == "__main__":
     arch, target = sys.argv[1], sys.argv[2]
     zipfile.ZipFile(rf".github\workflows\openssl_builds\openssl_{arch}.zip").extractall(f"C:\\")
+    print(os.listdir(f"C:\\"))
+    print(os.listdir(f"C:\\_install"))
     shutil.move(f"C:\\_install\\Program Files\\OpenSSL", f"C:\\OpenSSL_{arch}")
